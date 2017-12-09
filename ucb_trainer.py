7056f9acfd5dafc7c0a23a6916b67792114845b3
Tomasz Stęczniewski <tomaszsteczniewski@gmail.com>
2017-12-09 12:21:37 +0100

    Cost function.

--- ucb_trainer.py
+++ ucb_trainer.py
@@ -1,6 +1,8 @@
 from algorithms.ucb_algorithm import UCBAlgorithm
 from algorithms.vertical_lstm_ugts_algorithm import VerticalLSTMUGTSAlgorithm
 from games.omringa import OmringaGameState
+from torch.autograd import Variable
+from torch.nn import functional as F
 import numpy as np
 import torch
 
@@ -11,23 +13,31 @@ ucb_algorithm = UCBAlgorithm(game_state, 5, np.sqrt(2))
 ugts_algorithm = VerticalLSTMUGTSAlgorithm(
     game_state=game_state,
     grow_factor=5,
-    statistic_size=200,
-    update_size=200,
-    statistic_kernel_shapes=[(64, 2), (128, 2), (256, 2)],
-    statistic_hidden_features=[100, 100, 100],
-    update_hidden_features=[100, 100, 100],
-    modified_statistic_layers=4,
-    modified_update_layers=4,
+    statistic_size=100,
+    update_size=100,
+    statistic_kernel_shapes=[(32, 2)],
+    statistic_hidden_features=[100],
+    update_hidden_features=[100],
+    modified_statistic_layers=2,
+    modified_update_layers=2,
     move_rate_hidden_features=[100])
 
-for i in range(10):
-    print(i)
-    ucb_algorithm.improve()
+ucb_move_rates = []
+ugts_move_rates = []
 
-print(ucb_algorithm.tree[:20])
+for i in range(3000):
+    print(i)
+    for j in range(10):
+        ucb_algorithm.improve()
+    ucb_move_rates.extend(ucb_algorithm.move_rates())
 
 for i in range(3000):
     print(i)
     ugts_algorithm.improve()
+    ugts_move_rates.extend(ugts_algorithm.move_rates())
 
-print(ugts_algorithm.tree[:20])
+loss = F.binary_cross_entropy_with_logits(
+    torch.cat(ugts_move_rates, 0),
+    F.sigmoid(Variable(torch.from_numpy(np.array(ucb_move_rates)))))
+print(loss)
+loss.backward()
